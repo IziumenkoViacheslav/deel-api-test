3ced9f9cac5ffafd3e1554f390e6207e7a9bf5bc
IziumenkoViacheslav <2744097@gmail.com>
2022-12-08 18:50:45 +0100

    return contract only if it belongs to profile

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
   const { Contract } = req.app.get('models');
   const { id } = req.params;
   const contract = await Contract.findOne({
-    where: { id },
+    where: { id, ClientId: req.profile.dataValues.id },
   });
   if (!contract) return res.status(404).end();
   res.json(contract);
