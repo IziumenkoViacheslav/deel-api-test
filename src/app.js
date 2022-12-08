c992025016e1f86e23bb9d0b9bc24f26da15eb92
IziumenkoViacheslav <2744097@gmail.com>
2022-12-08 19:21:05 +0100

    rout contracts added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Op } = require('sequelize');
 const { sequelize } = require('./model');
 const { getProfile } = require('./middleware/getProfile');
+
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize);
@@ -19,5 +21,19 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
   });
   if (!contract) return res.status(404).end();
   res.json(contract);
+});
+app.get('/contracts', getProfile, async (req, res) => {
+  const { Contract } = req.app.get('models');
+  const contracts = await Contract.findAll({
+    where: {
+      status: { [Op.ne]: 'terminated' },
+      [Op.or]: [
+        { ClientId: req.profile.dataValues.id },
+        { ContractorId: req.profile.dataValues.id },
+      ],
+    },
+  });
+  if (contracts.lenght) return res.status(404).end();
+  res.json(contracts);
 });
 module.exports = app;
