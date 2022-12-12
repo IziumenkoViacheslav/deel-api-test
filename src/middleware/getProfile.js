a3713fb003972260b0ca1146a4c6d6a26cc2bc03
IziumenkoViacheslav <2744097@gmail.com>
2022-12-12 16:52:12 +0100

    fix logic, if profile not present return error

--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -1,7 +1,11 @@
 const getProfile = async (req, res, next) => {
   const { Profile } = require('../model');
+  const profileId = req.get('profile_id');
+  if (!profileId) {
+    res.json({ error: 'no profile_id in request' });
+  }
   const profile = await Profile.findOne({
-    where: { id: req.get('profile_id') || 0 },
+    where: { id: profileId },
   });
   if (!profile) return res.status(401).end();
   req.profile = profile;
