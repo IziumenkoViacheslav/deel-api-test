9901f32b435a7e5f3ec551a2da177f18388a5c54
IziumenkoViacheslav <2744097@gmail.com>
2022-12-12 18:28:18 +0100

    job pay added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Op } = require('sequelize');
-const { sequelize, Contract, Job } = require('./model');
+const { sequelize, Contract, Job, Profile } = require('./model');
 const { getProfile } = require('./middleware/getProfile');
 
 const app = express();
@@ -11,10 +11,14 @@ app.use(bodyParser.json());
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id', getProfile, async (req, res) => {
+app.get('/contracts/:id', async (req, res) => {
+  const profileId = req.get('profile_id');
   const { id } = req.params;
   const contract = await Contract.findOne({
-    where: { id, ClientId: req.profile.dataValues.id },
+    where: {
+      id,
+      [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }],
+    },
   });
   if (!contract) return res.status(404).end();
   res.json(contract);
@@ -53,37 +57,47 @@ app.get('/jobs/unpaid', getProfile, async (req, res) => {
 // TODO add transaction
 app.post('/jobs/:job_id/pay', async (req, res) => {
   const profileId = req.get('profile_id');
+  const profile = await Profile.findOne({ where: { id: profileId } });
 
   const { amount } = req.body;
   console.log({ amount });
   const job_id = req.params.job_id;
   console.log('job_id', req.params.job_id);
-  const balance = req.profile.dataValues.balance;
+  const balance = profile.balance;
   console.log({ balance });
   if (balance < amount) {
     return res.json({ error: 'user have no money for this payment' });
   }
-  if (req.profile.dataValues.type !== 'client') {
-    P;
+  if (profile.dataValues.type !== 'client') {
     return res.json({ error: 'only client can pay to the contractor' });
   }
-  const { Job } = req.app.get('models');
-  const { Contract } = req.app.get('models');
-  const { Profile } = req.app.get('models');
-  const clientProfileUpdated = await Profile.update({
-    where: { id: req.profile.dataValues.id },
-    balance: req.profile.dataValues.balance - amount,
-  });
-  // get balance of contractor
-  const cotractorProfileUpdated = await Profile.update({
-    balance: req.profile.dataValues.balance + amount,
-    include: {
-      model: Job,
-      wher: {
-        id: req.params.job_id,
-      },
+  console.log({ profile: profile.dataValues.id });
+  console.log(
+    'profile.dataValues.balance - amount',
+    profile.dataValues.balance - amount
+  );
+  const clientProfileUpdated = await Profile.update(
+    {
+      balance: profile.dataValues.balance - amount,
     },
+    {
+      where: { id: profile.dataValues.id },
+    }
+  );
+  const job = await Job.findOne({
+    where: { id: req.params.job_id },
+    include: { model: Contract },
   });
+  const contractorId = job.dataValues.Contract.dataValues.ContractorId;
+  console.log({ contractorId });
+  const cotractorProfileUpdated = await Profile.update(
+    {
+      balance: profile.dataValues.balance + amount,
+    },
+    {
+      where: { id: contractorId },
+    }
+  );
   res.json({ success: true });
 });
 module.exports = app;
